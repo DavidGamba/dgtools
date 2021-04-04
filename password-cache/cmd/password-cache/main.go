ca4712b266efe5d1cec3cd910f3fd24d2809989a
David Gamba <davidgamba@gmail.com>
2021-04-03 22:00:35 -0600

    password-cache: Update code to allow using as a library

---
+++ password-cache/cmd/password-cache/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	passwordcache "github.com/DavidGamba/dgtools/password-cache"
+	"github.com/DavidGamba/go-getoptions"
+)
+
+var logger = log.New(ioutil.Discard, "", log.LstdFlags)
+
+func main() {
+	os.Exit(program(os.Args))
+}
+
+func program(args []string) int {
+	var timeout int
+	opt := getoptions.New()
+	opt.Self("", `Saves/Retrieves passwords from/to the Linux keyring.
+	It will cache the password for a given timeout.
+	If a password doesn't exist in the keyring it will prompt the user for one.`)
+	opt.Bool("help", false, opt.Alias("?"))
+	opt.Bool("debug", false)
+	opt.Bool("print", false, opt.Description("Print password to STDOUT"))
+	opt.IntVar(&timeout, "timeout", 900, opt.ArgName("seconds"),
+		opt.Description("Timeout in seconds, default 15 minutes"))
+	opt.HelpSynopsisArgs("<key-name>")
+	remaining, err := opt.Parse(args[1:])
+	if opt.Called("help") {
+		fmt.Println(opt.Help())
+		return 1
+	}
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
+		return 1
+	}
+	if opt.Called("debug") {
+		logger.SetOutput(os.Stderr)
+	}
+	logger.Println(remaining)
+	if len(remaining) < 1 {
+		fmt.Fprintf(os.Stderr, "ERROR: Missing key\n")
+		fmt.Fprintln(os.Stderr, opt.Help(getoptions.HelpSynopsis))
+		return 1
+	}
+	keyName := remaining[0]
+
+	data, err := passwordcache.GetAndCacheSecret(keyName, "", uint(timeout))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
+		return 1
+	}
+	if opt.Called("print") {
+		fmt.Printf("%s", data)
+	}
+	return 0
+}
---
+++ password-cache/lib.go
@@ -0,0 +1,88 @@
+// Package passwordcache provides a utility to save secrets into the linux keyring
+package passwordcache
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"syscall"
+
+	"github.com/jsipprell/keyctl"
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+var logger = log.New(ioutil.Discard, "", log.LstdFlags)
+
+// GetSecret - Gets a secret from the User Session Keyring.
+// If the key doesn't exist, it asks the user to enter the password value.
+func GetSecret(name, msg string) ([]byte, error) {
+	if msg == "" {
+		msg = fmt.Sprintf("Enter password for '%s': ", name)
+	}
+
+	// Create session
+	keyring, err := keyctl.UserSessionKeyring()
+	if err != nil {
+		return nil, fmt.Errorf("couldn't create keyring session: %w", err)
+	}
+
+	// Retrieve
+	key, err := keyring.Search(name)
+	if err == nil {
+		data, err := key.Get()
+		if err != nil {
+			return nil, fmt.Errorf("couldn't retrieve key data: %w", err)
+		}
+		info, _ := key.Info()
+		logger.Printf("key: %+v", info)
+		return data, nil
+	}
+
+	// If not found promt user
+	fmt.Print(msg)
+	password, err := terminal.ReadPassword(int(syscall.Stdin))
+	fmt.Println()
+	if err != nil {
+		return nil, fmt.Errorf("failed to read secret: %w", err)
+	}
+	return password, nil
+}
+
+// CacheSecret - Saves a secret to the User Session Keyring.
+// It will cache the secret for a given number of seconds.
+//
+// To invalidate a secret, save it with a 1 second timeout.
+func CacheSecret(name string, password []byte, timeoutSeconds uint) error {
+	// Create session
+	keyring, err := keyctl.UserSessionKeyring()
+	if err != nil {
+		return fmt.Errorf("couldn't create keyring session: %w", err)
+	}
+
+	// Store key
+	keyring.SetDefaultTimeout(timeoutSeconds)
+	key, err := keyring.Add(name, password)
+	if err != nil {
+		return fmt.Errorf("couldn't store '%s': %s", name, err)
+	}
+	info, _ := key.Info()
+	logger.Printf("key: %+v", info)
+	return nil
+}
+
+// GetAndCacheSecret - Gets a secret from the User Session Keyring.
+// If the key doesn't exist, it asks the user to enter the password value.
+// It also saves the secret to the User Session Keyring.
+// It will cache the secret for a given number of seconds.
+//
+// To invalidate a secret, save it with a 1 second timeout.
+// Every read will refresh the cache timeout.
+func GetAndCacheSecret(name, msg string, timeoutSeconds uint) ([]byte, error) {
+	data, err := GetSecret(name, msg)
+	if err != nil {
+		return data, err
+	}
+
+	err = CacheSecret(name, data, timeoutSeconds)
+	return data, err
+}
