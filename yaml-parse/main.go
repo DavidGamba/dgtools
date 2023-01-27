60269cdde683c84e7e512a2722a9880d09c8d6b6
David Gamba <davidgamba@gmail.com>
2023-01-26 23:19:43 -0700

    yaml-parse: Update copyright year

--- yaml-parse/main.go
+++ yaml-parse/main.go
@@ -1,6 +1,6 @@
 // This file is part of dgtools.
 //
-// Copyright (C) 2019-2021  David Gamba Rios
+// # Copyright (C) 2019-2023  David Gamba Rios
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
