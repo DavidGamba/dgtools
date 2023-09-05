85dcc483337a14fd94234ac0f411169691693336
David Gamba <davidgamba@gmail.com>
2023-09-04 21:10:09 -0600

    homebrew: Update tz install

--- HomebrewFormula/tz.rb
+++ HomebrewFormula/tz.rb
@@ -1,8 +1,7 @@
 class Tz < Formula
   desc "Show time zones based on user defined groups"
   homepage "https://github.com/DavidGamba/dgtools/tree/master/tz"
-  url "https://github.com/DavidGamba/dgtools/archive/refs/tags/tz/v0.1.0.tar.gz"
-  sha256 "1ffaae8225ef3d7e3fdcf61348d2fb2b100bcd959cfaeed5ddeef7038c844786"
+  head "https://github.com/DavidGamba/dgtools.git", branch: "master"
 
   depends_on "go" => :build
 
@@ -12,8 +11,14 @@ class Tz < Formula
       system "go", "build"
       bin.install "tz"
     end
-    inreplace "HomebrewFormula/completions.bash", "tool", "tz"
-    bash_completion.install "HomebrewFormula/completions.bash"
+    cd "HomebrewFormula" do
+      inreplace "completions.bash", "tool", "tz"
+      inreplace "completions.zsh", "tool", "tz"
+      ohai "Installing bash completion..."
+      bash_completion.install "completions.bash" => "dgtools.tz.bash"
+      ohai "Installing zsh completion... (requires bashcompinit)"
+      zsh_completion.install "completions.zsh" => "dgtools.tz.zsh"
+    end
   end
 
   test do
