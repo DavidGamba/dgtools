6ec7908ba990a541feb6af3012be6910ced0e092
David Gamba <davidgamba@gmail.com>
2023-09-04 20:19:01 -0600

    homebrew: Add completion support

---
+++ HomebrewFormula/completions.bash
@@ -0,0 +1,1 @@
+complete -o default -C tool tool
--- HomebrewFormula/tz.rb
+++ HomebrewFormula/tz.rb
@@ -1,20 +1,22 @@
- class Tz < Formula
-   desc "Show time zones based on user defined groups"
-   homepage "https://github.com/DavidGamba/dgtools/tree/master/tz"
-   url "https://github.com/DavidGamba/dgtools/archive/refs/tags/tz/v0.1.0.tar.gz"
-   sha256 "1ffaae8225ef3d7e3fdcf61348d2fb2b100bcd959cfaeed5ddeef7038c844786"
+class Tz < Formula
+  desc "Show time zones based on user defined groups"
+  homepage "https://github.com/DavidGamba/dgtools/tree/master/tz"
+  url "https://github.com/DavidGamba/dgtools/archive/refs/tags/tz/v0.1.0.tar.gz"
+  sha256 "1ffaae8225ef3d7e3fdcf61348d2fb2b100bcd959cfaeed5ddeef7038c844786"
 
-   depends_on "go" => :build
+  depends_on "go" => :build
 
-   def install
-     cd "tz" do
-       system "go", "get"
-       system "go", "build"
-       bin.install "tz"
-     end
-   end
+  def install
+    cd "tz" do
+      system "go", "get"
+      system "go", "build"
+      bin.install "tz"
+    end
+    inreplace "HomebrewFormula/completions.bash", "tool", "tz"
+    bash_completion.install "HomebrewFormula/completions.bash"
+  end
 
-   test do
-     assert_match /Use 'tz help <command>' for extra details/, shell_output("#{bin}/tz")
-   end
- end
+  test do
+    assert_match /Use 'tz help <command>' for extra details/, shell_output("#{bin}/tz")
+  end
+end
