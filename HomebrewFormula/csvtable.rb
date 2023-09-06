f25f6a827713d91d11e18f4f66809e427bb62767
David Gamba <davidgamba@gmail.com>
2023-09-05 23:59:51 -0600

    homebrew: Add csvtable formula

---
+++ HomebrewFormula/csvtable.rb
@@ -0,0 +1,36 @@
+load './tool.rb'
+
+class Csvtable < Formula
+  @@tool = Tool.new("csvtable", "Pretty print CSV/TSV data", "clitable/cmd/csvtable")
+
+  desc "#{@@tool.desc}"
+  homepage "https://github.com/DavidGamba/dgtools/tree/master/#{@@tool.name}"
+  head "https://github.com/DavidGamba/dgtools.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    cd "#{@@tool.path}" do
+      system "go", "get"
+      system "go", "build"
+      bin.install "#{@@tool.name}"
+    end
+    cd "HomebrewFormula" do
+      inreplace "completions.bash", "tool", "#{@@tool.name}"
+      inreplace "completions.zsh", "tool", "#{@@tool.name}"
+      ohai "Installing bash completion..."
+      bash_completion.install "completions.bash" => "dgtools.#{@@tool.name}.bash"
+      ohai %{Installing zsh completion...
+      To enable zsh completion add this to your ~/.zshrc
+
+      \tsource #{zsh_completion.sub prefix, HOMEBREW_PREFIX}/dgtools.#{@@tool.name}.zsh
+      }
+      zsh_completion.install "completions.zsh" => "dgtools.#{@@tool.name}.zsh"
+      ohai "Installed #{@@tool.name} from #{@@tool.path} dir"
+    end
+  end
+
+  test do
+    assert_match /Use '#{@@tool.name} help[^']*' for extra details/, shell_output("#{bin}/#{@@tool.name} --help")
+  end
+end
