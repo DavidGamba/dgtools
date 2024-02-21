18cbb15a2edff6aaef95a3f163ae3ac6225a9b15
David Gamba <davidgamba@gmail.com>
2024-02-20 23:06:18 -0700

    bt: add test for build

---
+++ bt/terraform/build_test.go
@@ -0,0 +1,74 @@
+package terraform
+
+import (
+	"context"
+	"fmt"
+	"slices"
+	"strings"
+	"testing"
+
+	"github.com/DavidGamba/dgtools/bt/config"
+	"github.com/DavidGamba/dgtools/run"
+	"github.com/DavidGamba/go-getoptions"
+)
+
+func TestBuild(t *testing.T) {
+	t.Setenv("HOME", "/home/user")
+
+	t.Run("TestBuild without config", func(t *testing.T) {
+		buf := setupLogging()
+		ctx := context.Background()
+		cfg, _, _ := config.Get(ctx, "x")
+		ctx = config.NewConfigContext(ctx, cfg)
+		tDir := t.TempDir()
+		ctx = NewDirContext(ctx, tDir)
+		mock := run.CMDCtx(ctx).Mock(func(r *run.RunInfo) error {
+			if r.GetDir() != tDir {
+				return fmt.Errorf("unexpected dir: %s", r.GetDir())
+			}
+			op := r.Cmd[1]
+			switch op {
+			case "init":
+				if !slices.Equal(r.Cmd, []string{"terraform", "init", "-no-color"}) {
+					return fmt.Errorf("unexpected cmd: %v", r.Cmd)
+				}
+			case "plan":
+				if !slices.Equal(r.Cmd, []string{"terraform", "plan", "-out", ".tf.plan", "-no-color"}) {
+					return fmt.Errorf("unexpected cmd: %v", r.Cmd)
+				}
+			case "apply":
+				if !slices.Equal(r.Cmd, []string{"terraform", "apply", "-input", ".tf.plan-dev", "-no-color"}) {
+					return fmt.Errorf("unexpected cmd: %v", r.Cmd)
+				}
+			default:
+				return fmt.Errorf("unexpected cmd: %v", r.Cmd)
+			}
+			for _, e := range r.GetEnv() {
+				if strings.Contains(e, "TF_DATA_DIR") {
+					if e != "TF_DATA_DIR=.terraform" {
+						return fmt.Errorf("unexpected env: %v", e)
+					}
+				}
+			}
+			return nil
+		})
+		ctx = run.ContextWithRunInfo(ctx, mock)
+		opt := getoptions.New()
+		opt.String("profile", "default")
+		opt.String("ws", "")
+		opt.Bool("destroy", false)
+		opt.Bool("detailed-exitcode", false)
+		opt.Bool("ignore-cache", false)
+		opt.Bool("no-checks", false)
+		opt.Bool("apply", false)
+		opt.Bool("show", false)
+		opt.StringSlice("var-file", 1, 1)
+		opt.StringSlice("target", 1, 99)
+		opt.StringSlice("replace", 1, 99)
+		err := buildRun(ctx, opt, []string{})
+		if err != nil {
+			t.Errorf("TestBuild error: %s", err)
+		}
+		t.Log(buf.String())
+	})
+}
